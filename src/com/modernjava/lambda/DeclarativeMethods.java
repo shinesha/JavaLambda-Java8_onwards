9ab608687623eefdaf92cd3cc6b9a2b7c809f3e1
shinesha <jodrew_7@hotmail.com>
2021-04-07 21:27:18 +0100

    added some declarative examples

---
+++ src/com/modernjava/lambda/DeclarativeMethods.java
@@ -0,0 +1,24 @@
+package com.modernjava.lambda;
+
+public class DeclarativeMethods {
+
+
+//    //print the numbers
+//    IntStream numbers =IntStream.of(1,2,3,4,5);
+//      numbers.forEach(System.out.println);
+//
+//    // print the range
+//    InStream numbers=InstStream.rangeClosed(1,5);
+//      numbers.forEach(System.out::println)
+//
+//    //sum the numbers
+//    int sum = intStream.of(1,2,3,4,5).sum();
+//      System.out.println("sum="+sum);
+//
+//    //sum the range
+//    int sum=IntStream.rangeClosed(1,5).sum()
+//
+//    // minimum number
+//    List<Integer>numbers=Arrays.asList(1,2,3,4,5,6,7,8);
+//      numbers.Stream().min(Integer::compareTo);
+}

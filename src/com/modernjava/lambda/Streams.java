c0499fe618a72ea6235b0bb547d7b071c92b0295
shinesha <jodrew_7@hotmail.com>
2021-04-09 11:54:45 +0100

    exploring stream api

--- src/com/modernjava/lambda/Streams.java
+++ src/com/modernjava/lambda/Streams.java
@@ -1,7 +1,9 @@
 package com.modernjava.lambda;
 
+import java.net.SocketOption;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Streams {
@@ -28,6 +30,34 @@ public class Streams {
         // minimum number normally, done by checking if numbers is less than number save in a for loop
         List<Integer> numbers3 = Arrays.asList(-6, 1, 2, 3, 4, 5, 6, 7, 8);
         System.out.println(numbers3.stream().min(Integer::compareTo));
+
+
+        /*
+        INTERMEDIATE OPERATIONS ON STREAMS
+         */
+        //Map apply function to stream
+        List<Integer> numbers4 = Arrays.asList(2,3,4,5,6);
+        List<Integer> square = numbers4.stream().map(x->x*x).collect(Collectors.toList());
+        System.out.println(square);
+
+        // names begining with J
+        List<String> names = Arrays.asList("Syed", "Mike", "Jenny", "Rajeev");
+        List<String> result = names.stream()
+                .filter(s->s.startsWith("J"))
+                .collect(Collectors.toList());
+        result.forEach(System.out::println);
+
+        // sort the names array alphabetically
+        List result1 = names.stream()
+                .sorted()
+                .collect(Collectors.toList());
+        result1.forEach(System.out::println);
+
+
+        /*
+        TERMINAL OPERATIONS ON STREAMS
+         */
+
 
     }
 
